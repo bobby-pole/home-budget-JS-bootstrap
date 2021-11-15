0c93bf040be1888490cd34bac6a80f832a796275
robertlacheta <88884140+robertlacheta@users.noreply.github.com>
2021-11-15 16:03:37 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.addEventListener("DOMContentLoaded", () => {
   const incomeList = document.querySelector("#incomes-list");
   const incomeForm = document.querySelector("#incomes-form");
-  const incomeName = incomeList.querySelector("#income-name");
-  const incomeValue = incomeList.querySelector("#income-amount");
+  const incomeName = incomeForm.querySelector("#income-name");
+  const incomeValue = incomeForm.querySelector("#income-amount");
 });
 
 function addIncome(text) {
