7de4cd266cbdf3d834ad5fd28da00e0e4da4dc63
robertlacheta <88884140+robertlacheta@users.noreply.github.com>
2021-11-15 16:06:34 +0100

    Update script.js

--- script.js
+++ script.js
@@ -3,17 +3,17 @@ document.addEventListener("DOMContentLoaded", () => {
   const incomeForm = document.querySelector("#incomes-form");
   const incomeName = incomeForm.querySelector("#income-name");
   const incomeValue = incomeForm.querySelector("#income-amount");
-});
 
-function addIncome(text) {
-  console.log("Dodaj przychód");
-}
-
-incomeForm.addEventListener("submit", (e) => {
-  e.preventDefault();
-
-  if (incomeName.value !== "") {
-    addIncome(incomeName.value);
-    incomeName.value === "";
+  function addIncome(text) {
+    console.log("Dodaj przychód");
   }
+
+  incomeForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (incomeName.value !== "") {
+      addIncome(incomeName.value);
+      incomeName.value = "";
+    }
+  });
 });
